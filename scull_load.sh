a187befb56f132233964eb02bd970a3a0c07af21
Pushpendra Kumar <pushpendra.gangwar@outlook.com>
2019-12-15 20:32:24 +0100

    Make scull load script generic

--- scull_load.sh
+++ scull_load.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 module="scull"
 device="scull"
-mode="664"
+MODE="664"
 
 # invoke insmod with all arguments we got
 # and use a pathname, as newer modutils don't look in . by default
@@ -15,10 +15,7 @@ echo "Major is $major"
 
 mknod /dev/${device}0 c $major 0
 
-# give appropriate group/permissions, and change the group.
-# Not all distributions have staff, some have "wheel" instead.
-group="pkumar"
-grep -q '^pkumar:' /etc/group || echo "Group pkumar doesn't exists"
+grep -q "^${USER}:" /etc/group || echo "Group ${USER} doesn't exists"
 
-chgrp $group /dev/${device}0
-chmod $mode /dev/${device}0
+chgrp $USER /dev/${device}0
+chmod $MODE /dev/${device}0
